22fbbca8ab16081c5047293b1480240bf690ecbf
109taha <109taha@gmail.com>
2023-09-19 10:08:37 -0700

    plansta added

--- src/server.js
+++ src/server.js
@@ -29,8 +29,10 @@ app.get("/", (req, res) => {
 const UserRouter = require("./router/user");
 const ComplaintRouter = require("./router/complaint");
 const VisitsDaily = require("./router/Visits");
+const Plant = require("./router/plant")
 
 //router
 app.use("/auth", UserRouter);
 app.use("/v1", ComplaintRouter);
 app.use("/visit", VisitsDaily);
+app.use("/plants", Plant)
