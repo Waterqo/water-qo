c6f27ac049bd0121573709d4cad7fb0b6fa08abf
109taha <109taha@gmail.com>
2023-10-30 23:08:43 +0500

    inventory manager flow completed

--- src/router/user.js
+++ src/router/user.js
@@ -8,6 +8,7 @@ const {
 const bcrypt = require("bcrypt");
 const JWT = require("jsonwebtoken");
 const Admin = require("../models/admin");
+const InvManager = require("../models/inventerymanage");
 const Staff = require("../models/staff");
 const Client = require("../models/clientSchema");
 const {
@@ -123,6 +124,70 @@ router.post("/register/admin", AdminJoiSchema, async (req, res) => {
   }
 });
 
+router.post("/register/Invmanager", AdminJoiSchema, async (req, res) => {
+  try {
+    const { name, contact_number, email, password } = req.body;
+    if (!name || !contact_number || !email || !password) {
+      return res.status(400).send({
+        success: false,
+        message: "Kindly provide complete information.",
+      });
+    }
+
+    const existingUser = await Client.findOne({ email: req.body.email });
+    if (existingUser) {
+      return res.status(400).send({
+        success: false,
+        message: "The email is already registered Client!",
+      });
+    }
+    const existingUserAdmin = await Admin.findOne({ email: req.body.email });
+    if (existingUserAdmin) {
+      return res.status(400).send({
+        success: false,
+        message: "The email is already registered as Admin!",
+      });
+    }
+    const existingUserStaff = await Staff.findOne({ email: req.body.email });
+    if (existingUserStaff) {
+      return res.status(400).send({
+        success: false,
+        message: "The email is already registered as Staff!",
+      });
+    }
+    const existingUserManager = await InvManager.findOne({
+      email: req.body.email,
+    });
+    if (existingUserStaff) {
+      return res.status(400).send({
+        success: false,
+        message: "The email is already registered as Staff!",
+      });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    req.body.password = hashedPassword;
+
+    const user = new InvManager({
+      name,
+      contact_number,
+      email,
+      password: hashedPassword,
+      role: "Inventory Manager",
+    });
+    await user.save();
+    res.status(200).send({
+      success: true,
+      message: "Inventory Manager is Register Successfully",
+      user,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error: " + error.message);
+  }
+});
+
 router.post("/register/Staff", StaffJoiSchema, async (req, res) => {
   try {
     const { name, contact_number, username, password } = req.body;
@@ -199,6 +264,31 @@ router.post("/login", async (req, res) => {
         message: "Admin login successfully",
         token,
         data: user,
+      });
+    }
+    const Invmanager = await InvManager.findOne({ email: req.body.email });
+    if (Invmanager) {
+      const validPassword = await bcrypt.compare(
+        req.body.password,
+        Invmanager.password
+      );
+      if (!validPassword) {
+        return res.status(400).send({
+          success: false,
+          message: "Maybe your Email or Password is not correct!",
+        });
+      }
+
+      const token = JWT.sign(
+        { userId: Invmanager._id },
+        process.env.JWT_SEC_INVMANAGER
+      );
+
+      return res.status(200).send({
+        success: true,
+        message: "Inventery Manager login successfully",
+        token,
+        data: Invmanager,
       });
     }
     const anotherUser = await Client.findOne({ email: req.body.email });
@@ -308,6 +398,21 @@ router.get("/find/admin", async (req, res) => {
   }
 });
 
+router.get("/find/manager", async (req, res) => {
+  try {
+    const allUser = await InvManager.find();
+    if (!allUser.length > 0) {
+      return res
+        .status(400)
+        .send({ seccess: false, message: "No Admin Found" });
+    }
+    res.status(200).send({ success: true, data: allUser });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error: " + error.message);
+  }
+});
+
 router.get("/find/staff", async (req, res) => {
   try {
     const allUser = await Staff.find();
@@ -374,6 +479,35 @@ router.put("/update/admin", verifyAdmin, async (req, res) => {
     const userId = req.user;
     const { name, contact_number, email, password, deviceToken } = req.body;
     const user = await Admin.findById(userId);
+
+    user.name = name || user.name;
+    user.contact_number = contact_number || user.contact_number;
+    user.email = email || user.email;
+    user.deviceToken = deviceToken || user.deviceToken;
+
+    if (password) {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
+      user.password = hashedPassword;
+    }
+
+    await user.save();
+    res.status(200).send({
+      success: true,
+      message: "Admin Updated Successfully",
+      data: user,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error: " + error.message);
+  }
+});
+
+router.put("/update/manager/:userId", async (req, res) => {
+  try {
+    const { name, contact_number, email, password, deviceToken } = req.body;
+    const userId = req.params.userId;
+    const user = await InvManager.findById(userId);
 
     user.name = name || user.name;
     user.contact_number = contact_number || user.contact_number;
